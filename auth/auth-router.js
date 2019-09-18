10033f0fe0e8911310f5b025d6eb9aecdd39d8e0
Pat Palmerston <patpalmerston@gmail.com>
2019-09-17 22:18:15 -0700

    registration working with token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 
+const tokenService = require('./token-service');
+
 const Users = require('../users/users-model');
 
 const router = express.Router();
@@ -12,7 +14,11 @@ router.post('/register', (req, res) => {
 
 	Users.add({ username, password: hash })
 		.then(saved => {
-			res.status(201).json(saved);
+			const token = tokenService.generateToken(saved);
+			res.status(201).json({
+				message: `Hello ${saved.username} you are registered!`,
+				token
+			});
 		})
 		.catch(err => {
 			console.log(err);
@@ -20,39 +26,42 @@ router.post('/register', (req, res) => {
 		});
 });
 
-router.post('/login', (req, res) => {
-	let { username, password } = req.body;
-	
-	Users.findBy({ username })
-	.first()
-	.then(user => {
-		console.log(req.session.user);
-			if (user && bcrypt.compareSync(password, user.password)) {
-				req.session.user = user;
-				console.log('after set user', req.session)
-				return res.status(200).json({ message: `Welcome ${user.username}!` });
-			} else {
-				res.status(401).json({ message: 'Invalid Credentials' });
-			}
-		})
-		.catch(err => {
-			console.log(err);
-			res.status(500).json({ error: 'could not find user' });
-		});
-});
+// router.post('/login', (req, res) => {
+// 	let { username, password } = req.body;
 
-router.get('/logout', (req, res) => {
-	if (req.session) {
-		req.session.destroy(err => {
-			if (err) {
-				res.send('unable to log out');
-			} else {
-				res.send('later gator');
-			}
-		});
-	} else {
-		res.end().send('you were never logged in');
-	}
-});
+// 	Users.findBy({ username })
+// 		.first()
+// 		.then(user => {
+// 			if (user && bcrypt.compareSync(password, user.password)) {
+// 				const token = tokenService.generateToken(user);
+
+// 				return res
+// 					.status(200)
+// 					.json({ message: `Welcome ${user.username}!`, token });
+// 			} else {
+// 				res.status(401).json({ message: 'Invalid Credentials' });
+// 			}
+// 		})
+// 		.catch(err => {
+// 			console.log(err);
+// 			res.status(500).json({ error: 'could not find user' });
+// 		});
+// });
+
+// Not sure about the logout conversion yet
+
+// router.get('/logout', (req, res) => {
+// 	if (req.session) {
+// 		req.session.destroy(err => {
+// 			if (err) {
+// 				res.send('unable to log out');
+// 			} else {
+// 				res.send('later gator');
+// 			}
+// 		});
+// 	} else {
+// 		res.end().send('you were never logged in');
+// 	}
+// });
 
 module.exports = router;
